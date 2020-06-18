563d7ed7c1db81b88bddf31c79ce1522c24ab885
AssanSav <assane.savadogo81@gmail.com>
2020-06-18 17:02:49 -0400

    Fetch user received messages action creator

---
+++ src/actions/fetchReceivedMessages.js
@@ -0,0 +1,20 @@
+import { FETCH_RECEIVED_MESSAGES } from "./types"
+
+
+export const fetchReceivedMessages = () => {
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/messages-inbox", {
+            headers: {
+                "Conent-Type": "application/json",
+                "Accept": "application/json"
+            },
+            credentials: "include"
+            })
+            .then(resp => resp.json())
+            .then(data => {
+                dispatch({
+                    type: FETCH_RECEIVED_MESSAGES, receivedMessages: data.received_messages.data.map(m => m.attributes)})
+            
+        })
+    }
+}
