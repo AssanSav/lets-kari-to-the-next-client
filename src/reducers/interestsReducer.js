17889f8dae9a7298f4dbc2dde715fc0fa04d920c
AssanSav <assane.savadogo81@gmail.com>
2020-06-18 17:17:43 -0400

    User interests reducer

---
+++ src/reducers/interestsReducer.js
@@ -0,0 +1,55 @@
+import { FETCH_INTERESTS, LOGGED_IN, SIGNUP, LOGIN, EDIT_PROFILE, FETCH_PROFILE, UPLOAD_PHOTO } from "../actions/types";
+
+const interestsReducer = (state = { interests: [], profileInterests: [] }, action) => {
+    const { payload, interests, type } = action 
+    
+    switch (type) {
+        case FETCH_INTERESTS: 
+            return {
+                ...state,
+                interests: payload
+            }
+        
+        case LOGGED_IN: 
+            return {
+                ...state,
+                interests: interests
+            }
+        
+        case SIGNUP:
+            return {
+                ...state,
+                interests: interests
+            }
+        
+        case EDIT_PROFILE:
+            return {
+                ...state,
+                interests: interests
+            }
+        
+        case FETCH_PROFILE: 
+            return {
+                ...state,
+                profileInterests: interests
+            }
+        
+        case LOGIN: 
+            return {
+                ...state,
+                interests: interests
+            }
+        
+        case UPLOAD_PHOTO:
+            return {
+                ...state,
+                interests: interests
+            }
+        
+        default: {
+            return state
+            }
+    }
+}
+
+export default interestsReducer 
