2fd4b94aa6212b3afa5863c8daf6b8a75128e833
AssanSav <assane.savadogo81@gmail.com>
2020-06-18 17:21:51 -0400

    Users reducer

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,73 @@
+import { SIGNUP, LOGGED_OUT, LOGGED_IN, LOGIN, LOGOUT, EDIT_PROFILE, FETCH_PROFILE, FETCH_USERS, UPLOAD_PHOTO} from "../actions/types"
+
+
+const usersReducer = (state = { status: false, user: {}, profile: {}, users: [] }, action) => {
+    const { payload, user, users, type } = action
+    switch (type) {
+        case SIGNUP:
+            return {
+            ...state,
+            status: true,
+            user: payload
+            }
+        
+        case FETCH_USERS: 
+            return {
+            ...state,
+            users: users
+            }
+
+        case LOGIN:
+            return {
+            ...state,
+            status: true,
+            user: user
+            }
+
+        case LOGGED_IN:
+            return {
+            ...state,
+            status: true,
+            user: user
+            }
+        
+        case EDIT_PROFILE: 
+            return {
+            ...state,
+            user: payload
+            }
+
+        case UPLOAD_PHOTO:
+            return {
+            ...state,
+            status: true,
+            user: payload
+            }
+        
+        case FETCH_PROFILE: 
+            return {
+            ...state,
+            profile: payload
+            }
+        
+        case LOGGED_OUT:
+            return {
+            ...state,
+            status: false,
+            user: {}
+            }
+
+        case LOGOUT:
+            return {
+            ...state,
+            status: false,
+            user: {}
+            }
+
+        default: {
+            return state
+            }
+    }
+}
+
+export default usersReducer
