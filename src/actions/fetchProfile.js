9f491efd16c80585f197e5f80b8461ccf9244554
AssanSav <assane.savadogo81@gmail.com>
2020-06-18 17:00:09 -0400

    Fetch user profile action creator

---
+++ src/actions/fetchProfile.js
@@ -0,0 +1,18 @@
+import { FETCH_PROFILE } from './types'
+
+
+export const fetchProfile = (id) => {
+    return dispatch => {
+      return fetch(`http://localhost:3001/api/v1/users/${id}`, {
+        headers: {
+          "Content-type": "application/json",
+          "Accept": "application/json"
+        },
+        credentials: "include"
+        })
+        .then(resp => resp.json())
+        .then(data => {
+          dispatch({ type: FETCH_PROFILE, payload: data.user.data.attributes, interests: data.interests })
+        })
+    }
+}
