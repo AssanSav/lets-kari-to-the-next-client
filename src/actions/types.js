46c9175fc0e938468befebcb09961b6e3c6ed706
AssanSav <assane.savadogo81@gmail.com>
2020-06-18 17:06:15 -0400

    action types constants

---
+++ src/actions/types.js
@@ -0,0 +1,20 @@
+export const LOGGED_IN = "LOGGED_IN"
+export const LOGGED_OUT = "LOGGED_OUT"
+export const SIGNUP = "SIGNUP"
+export const LOGOUT = "LOGOUT"
+export const LOGIN = "LOGIN"
+
+export const FETCH_USERS = "FETCH_USERS"
+
+export const EDIT_PROFILE = "EDIT_PROFILE"
+export const FETCH_PROFILE = "FETCH_PROFILE"
+export const UPLOAD_PHOTO = "UPLOAD_PHOTO"
+
+export const FETCH_INTERESTS = "FETCH_INTERESTS"
+
+export const FETCH_MATCHES = "FETCH_MATCHES"
+
+export const CREATE_MESSAGE = "CREATE_MESSAGE"
+export const FETCH_SENT_MESSAGES = "FETCH_SENT_MESSAGES"
+export const FETCH_RECEIVED_MESSAGES = "FETCH_RECEIVED_MESSAGES"
+
