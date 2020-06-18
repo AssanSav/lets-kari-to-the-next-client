d43d742d9d89bf408ef18fd0dafa0952a428f965
AssanSav <assane.savadogo81@gmail.com>
2020-06-18 16:57:20 -0400

    Create new text message action creator

---
+++ src/actions/createMessage.js
@@ -0,0 +1,20 @@
+import { CREATE_MESSAGE } from "./types"
+
+
+export const createMessage = (formData) => {
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/messages", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                "Accept": "application/json"
+            },
+            credentials: "include",
+            body: JSON.stringify(formData)
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                dispatch({ type: CREATE_MESSAGE, message: data.message })
+            })
+    }
+}
