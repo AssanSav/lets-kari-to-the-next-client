d366bff66ad71deeff029b340feec903c2e83186
AssanSav <assane.savadogo81@gmail.com>
2020-06-18 17:18:56 -0400

    Combined reducers index

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux"
+import usersReducer from "./usersReducer"
+import messagesReducer from "./messagesReducer"
+import interestsReducer from "./interestsReducer"
+import matchesReducer  from "./matchesReducer"
+
+
+export const rootReducer = combineReducers({
+    usersReducer,
+    messagesReducer,
+    interestsReducer,
+    matchesReducer
+})
