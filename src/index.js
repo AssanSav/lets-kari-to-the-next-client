d32d40db10e15e356eeaf156a086bbf8626c0446
AssanSav <assane.savadogo81@gmail.com>
2020-06-18 17:20:40 -0400

    Index grand parent component setting up the redux store, dev tools and rendering app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware, compose } from "redux"
+import {Provider} from "react-redux"
+import logger from "redux-logger"
+import thunk from "redux-thunk"
+import {rootReducer} from "./reducers"
 import App from './App';
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+let store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk, logger)))
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode> 
+  ,
   document.getElementById('root')
 );
 
