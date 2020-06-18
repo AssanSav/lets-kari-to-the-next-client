44f5a47b5efd8b3efb9b1bc63aa11dbeeb92fd44
AssanSav <assane.savadogo81@gmail.com>
2020-06-18 17:18:22 -0400

    User matches reducer

---
+++ src/reducers/matchesReducer.js
@@ -0,0 +1,19 @@
+import { FETCH_MATCHES } from "../actions/types"
+
+
+const matchesReducer = (state = { matches: [] }, action) => {
+    const {payload, type} = action
+    
+    switch (type) {
+        case FETCH_MATCHES: 
+            return {
+                ...state,
+                matches: payload
+            }
+        
+        default: 
+            return state
+        }
+}
+
+export default matchesReducer
