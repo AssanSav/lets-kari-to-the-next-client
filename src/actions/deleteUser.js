11205e0027e7e04e9a26183886dbc257bee67446
AssanSav <assane.savadogo81@gmail.com>
2020-06-23 20:28:16 -0400

    Delete users action controller

---
+++ src/actions/deleteUser.js
@@ -0,0 +1,20 @@
+import { DELETE_USER } from "./types"
+
+
+export const deleteUser = (profile) => {
+    return dispatch => {
+        return fetch(`http://localhost:3001/api/v1/users/${profile.id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-type": "application/json",
+                "Accept": "application/json"
+            },
+            credentials: "include",
+            body: JSON.stringify(profile)
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                dispatch({ type: DELETE_USER, payload: data.user.data.attributes, interests: data.interests })
+            })
+    }
+}
