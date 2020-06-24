280b7697805894563bded2c8770d8f18e7bc46e2
AssanSav <assane.savadogo81@gmail.com>
2020-06-23 20:34:24 -0400

    actions types dispatched to update the redux store'

--- src/reducers/messagesReducer.js
+++ src/reducers/messagesReducer.js
@@ -1,8 +1,8 @@
-import { CREATE_MESSAGE, FETCH_RECEIVED_MESSAGES, FETCH_SENT_MESSAGES } from "../actions/types"
+import { CREATE_MESSAGE, FETCH_RECEIVED_MESSAGES, FETCH_SENT_MESSAGES, DELETE_SENT_MESSAGE, DELETE_RECEIVED_MESSAGE } from "../actions/types"
 
 
 const messagesReducer = (state = { message: {}, receivedMessages: [], sentMessages: [] }, action) => {
-    const {message, receivedMessages, sentMessages, type} = action
+    const { message, receivedMessages, sentMessageToDelete, receivedMessageToDelete, sentMessages, type} = action
     
     switch (type) {
         case CREATE_MESSAGE: 
@@ -22,7 +22,19 @@ const messagesReducer = (state = { message: {}, receivedMessages: [], sentMessag
                     ...state,
                     ...sentMessages,
                     receivedMessages: receivedMessages
-                }
+            }
+        
+        case DELETE_SENT_MESSAGE: 
+            return {
+                ...state,
+                sentMessages: state.sentMessages.filter(m => m.id !== sentMessageToDelete.id)
+            }
+        
+        case DELETE_RECEIVED_MESSAGE:
+            return {
+                ...state,
+                receivedMessages: state.receivedMessages.filter(m => m.id !== receivedMessageToDelete.id)
+            }
             
         default: 
             return state
