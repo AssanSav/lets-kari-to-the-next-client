ab70ee0126122d39ba47ee0c48c7791e13e2f862
AssanSav <assane.savadogo81@gmail.com>
2020-06-26 21:58:14 -0400

    Refact

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,5 +1,5 @@
-export const BASE_URL ="http://localhost:3001"
-    // "https://lets-kari-to-the-next.herokuapp.com"
+export const BASE_URL = "https://lets-kari-to-the-next.herokuapp.com"
+    // "http://localhost:3001"
 
 
 
