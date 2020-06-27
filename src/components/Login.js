fa0d43c52b01ab16d20cdbabc8e2582e9021c75b
AssanSav <assane.savadogo81@gmail.com>
2020-06-26 21:27:13 -0400

    Input fields validation and notifications

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,38 +9,74 @@ class Login extends Component {
     constructor() {
         super()
         this.state = {
-            email: "",
-            password: ""
+            fields: {},
+            errors: {}
         }
         this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
+        this.submituserRegistrationForm = this.submituserRegistrationForm.bind(this);
     }
 
+    submituserRegistrationForm(e) {
+        e.preventDefault();
+        if (this.validateForm()) {
+            let fields = {};
+            fields["email"] = "";
+            fields["password"] = "";
+            this.setState({
+                fields: fields
+            });
+        }
+        this.props.loginUser({ email: this.state.fields.email, password: this.state.fields.password })
+    }
 
     handleChange(e) {
+        let fields = this.state.fields
+        fields[e.target.name] = e.target.value
         this.setState({
-            [e.target.name]: e.target.value
+            fields
         })
     }
 
 
-    handleSubmit(e) {
-        e.preventDefault()
-        this.props.loginUser(this.state)
+    validateForm() {
+        let fields = this.state.fields;
+        let errors = {};
+        let formIsValid = true;
+
+        if (!fields["email"]) {
+            formIsValid = false;
+            errors["email"] = "*Please enter your email-ID.";
+        }
+        if (typeof fields["email"] !== "undefined") {
+            var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+            if (!pattern.test(fields["email"])) {
+                formIsValid = false;
+                errors["email"] = "*Please enter valid email-ID.";
+            }
+        }
+        if (!fields["password"]) {
+            formIsValid = false;
+            errors["password"] = "*Please enter your password.";
+        }
+        if (typeof fields["password"] !== "undefined") {
+            if (!fields["password"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
+                formIsValid = false;
+                errors["password"] = "*Wrong Password!";
+            }
+        }
 
         this.setState({
-            email: '',
-            password: ''
-        })
+            errors: errors
+        });
+        return formIsValid;
     }
-
 
     render() {
         const {email, password} = this.state
         return (
             <div className="form">
                 <h1>Login</h1>
-                <Form onSubmit={this.handleSubmit}>
+                <Form onSubmit={this.submituserRegistrationForm}>
                     <Form.Group>
                         <Form.Control
                             placeholder="email"
@@ -48,6 +84,7 @@ class Login extends Component {
                             name="email"
                             value={email}
                             onChange={this.handleChange} />
+                        <div style={{color: "red"}}>{this.state.errors.email}</div>
                     </Form.Group>
                     
                     <Form.Group>
@@ -58,6 +95,7 @@ class Login extends Component {
                             autoComplete={password}
                             value={password}
                             onChange={this.handleChange} />
+                        <div style={{ color: "red" }}>{this.state.errors.password}</div>
                     </Form.Group> 
                     <Button variant="outline-success" type="submit">Login</Button>
                 </Form>
