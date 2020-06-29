43f75d24e60b78c0b1b5c77f99e84629fe401440
AssanSav <assane.savadogo81@gmail.com>
2020-06-29 00:09:50 -0400

    Start with create-react-app

--- src/actions/sessionStatus.js
+++ src/actions/sessionStatus.js
@@ -7,7 +7,7 @@ export const sessionStatus = () => {
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "application/json",
-                // "Access-Control-Allow-Origin": "*"
+                "Access-Control-Allow-Origin": "https://lets-meetup-app.herokuapp.com/"
             },
             credentials: "include"
         })
