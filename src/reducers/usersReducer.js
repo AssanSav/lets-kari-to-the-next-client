ad14a4b89490b3a5699aa0417a75f41e89523908
AssanSav <assane.savadogo81@gmail.com>
2020-07-01 13:39:08 -0400

    Styling and refact

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -52,21 +52,18 @@ const usersReducer = (state = { status: false, user: {}, profile: {}, users: [] 
       
       case LOGGED_OUT:
           return {
-          ...state,
           status: false,
           user: {}
           }
 
       case LOGOUT:
           return {
-          ...state,
           status: false,
           user: {}
           }
 
       case DELETE_USER: 
           return {
-              ...state,
               status: false,
               user: {}
           }
