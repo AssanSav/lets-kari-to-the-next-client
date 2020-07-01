75e95f1d44bab0dc6851d1181e08ce331f031a85
AssanSav <assane.savadogo81@gmail.com>
2020-06-30 22:00:14 -0400

    Users chat room added

--- src/components/CreateMessage.js
+++ src/components/CreateMessage.js
@@ -1,60 +1,84 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
 import { createMessage } from "../actions/createMessage"
-import {Form, Button} from "react-bootstrap"
+import {fetchMessages} from "../actions/fetchMessages"
+import ReactDOM from "react-dom"
 
 
 class CreateMessage extends Component {
-    constructor() {
-        super()
-        this.state = {
-            content: "",
-            match_id: ""
-        }
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.handleChange = this.handleChange.bind(this)
-    }
+  constructor() {
+      super()
+      this.state = {
+          content: "",
+          match_id: ""
+      }
+      this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-    handleChange(e) {
+  componentDidMount() {
+    this.props.fetchMessages();
+    this.scrollToBot()
+  }
+
+  componentDidUpdate() {
+    this.scrollToBot()
+  }
+
+  scrollToBot() {
+    ReactDOM.findDOMNode(this.refs.chats).scrollTop = ReactDOM.findDOMNode(this.refs.chats).scrollHeight;
+  }
+
+  handleChange(e) {
+      this.setState({
+          content: e.target.value,
+          match_id: this.props.routerProps.match.params.id
+
+      })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    this.props.createMessage(this.state)
+      .then(() => {
         this.setState({
-            content: e.target.value,
-            match_id: this.props.routerProps.match.params.id
-
+          content: ""
         })
-    }
+        this.props.fetchMessages()
+      })
+  }
 
-    handleSubmit(e) {
-        e.preventDefault()
-        this.props.createMessage(this.state)
-        .then(() => {
-            this.props.routerProps.history.push("/sent-messages")
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group className="border border-primary mx-10">
-                        <Form.Control
-                            rows="4" cols="10"
-                            as="textarea"
-                            name="content"
-                            value={this.state.content}
-                            onChange={this.handleChange}
-                        />
-                    </Form.Group>
-                    <Button variant="outline-success" type="submit">Send</Button>
-                </Form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="chatroom">
+        <h3>Chat Room</h3>
+        <ul className="chats" ref="chats">
+          {
+            this.props.messages.map((message) =>
+              <li key={message.id} className={`chat ${this.props.user.user.id === message.match_id ? "right" : "left"}`}>
+                {this.props.user.id !== message.user_id
+                  && <img src={message.image} alt={`${message.username}'s profile pic`} />
+                }
+                {message.content}
+              </li>
+            )
+          }
+        </ul>
+        <form className="input" onSubmit={this.handleSubmit}>
+          <input name="content" value={this.state.content} type="texterea" ref="msg" onChange={this.handleChange} />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+      )
+  }
 }
 
-const mapStateToProps = ({ usersReducer, ownProps }) => {
-    return {
-        user: usersReducer
-    }
+const mapStateToProps = ({ usersReducer, messagesReducer }) => {
+  return {
+    user: usersReducer,
+    messages: messagesReducer.messages
+  }
 }
 
-export default connect(mapStateToProps, {createMessage})(CreateMessage)
+export default connect(mapStateToProps, {createMessage, fetchMessages})(CreateMessage)
