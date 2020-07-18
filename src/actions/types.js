47234e0570efea8c7e784de93bcd1a96ef701854
AssanSav <assane.savadogo81@gmail.com>
2020-07-18 14:19:50 -0400

    Device viewport scaled

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://lets-meetup-app-api.herokuapp.com";
+export const BASE_URL = "http://localhost:3001";
     // "https://lets-meetup-app-api.herokuapp.com"
     //"http://localhost:3001"
 
