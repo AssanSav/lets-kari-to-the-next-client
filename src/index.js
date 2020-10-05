c010b6a57fa870fa43b688b8395df09cc6a86721
AssanSav <assane.savadogo81@gmail.com>
2020-10-04 21:03:31 -0400

    Refact

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import thunk from "redux-thunk";
 import { rootReducer } from "./reducers";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 let store = createStore(
   rootReducer,
-  composeEnhancer(applyMiddleware(thunk, logger))
+  // composeEnhancer(applyMiddleware(thunk, logger))
+  applyMiddleware(thunk)
 );
 
 ReactDOM.render(
